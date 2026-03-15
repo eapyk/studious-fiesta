122c0a194d029f6cf350493de7317edb4baedecc
Disala Sennath <sennathdesilva@gmail.com>
2026-03-15 12:27:50 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+x = [2,3,4,5,6,7,7,4]
+y = [2,4,6,2,6,7,7,4]
+# x.append(600) 
+# x.insert(0,200)
+# print(x)
+# x.remove(200)
+# x.pop(4)
+# print(x)
+
+# z = x + y
+# print(z)
+
+# print(3 in z)
+
+
+if 3 in x:
+    print('Welcome to my world!')
+
