5d4ef3b0af298e394029646630f95410b58b022b
disala sennath <sennathdesilva@gmail.com>
2026-05-24 16:55:52 +0530

    new commit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-x = [2,3,4,5,6,7,7,4]
-y = [2,4,6,2,6,7,7,4]
+# a = [] #list
+# b = {} #Dictionary
+# c = () #tuple
+# d = {} #set
+
+# x = [2,3,4,5,6,7,7,4]
+# y = [2,4,6,2,6,7,7,4]
 # x.append(600) 
 # x.insert(0,200)
 # print(x)
@@ -13,6 +18,34 @@ y = [2,4,6,2,6,7,7,4]
 # print(3 in z)
 
 
-if 3 in x:
-    print('Welcome to my world!')
+# if 3 in x:
+#     print('Welcome to my world!')
+
+
+
+#Dictionaries
+
+# x = {"1000":"Disala","2000":"David"}
+# x["3000"] = "Yevon"
+# x[0] = "Zero"
+# x["Random"] = {1:"Hello",2:"World"}
+
+# y = x["Random"]
+# print(y.keys())
+
+
+# x = {1:"A",2:"B"}
+# y = x.get(4,"No Key founded!")
+# del x[2]
+# print(y)
+
+# x = {
+#     "a": ["Hello","Hi","Good Morning"],
+#     "b": ["Bye","Good night"]
+# }
+
+# y = x["a"]
+# print(y)
+
+# sets
 
